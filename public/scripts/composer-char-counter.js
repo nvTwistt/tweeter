11d23d2cb4ee121c67563c425b3538908cc9a7e2
nv_twistt <mtamkee@icloud.com>
2021-06-15 12:41:14 -0600

    added jQuery to decrement character count while typing. If user exceeds max amount, counter will turn red.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,28 @@
+//const { text } = require("express");
+
+$(document).ready(function() {
+  // --- our code goes here ---
+  console.log("ready");
+  const submitButton = document.getElementById("submitButton");
+  $(submitButton).on('click', function() {
+    console.log(this); //The this keyword is a reference to the button
+    console.log("clicked");
+  });
+  const textArea = document.getElementById("tweet-text");
+  const charCount = document.getElementsByClassName("counter");
+  // $("#btn").on('click', () => {
+  //   console.log(this); //The this keyword here refers to something else!
+  // });
+  $(textArea).keyup(function(){
+    $(charCount).text((140 - $(this).val().length));
+    const numChar = $(charCount).text();
+    if (numChar < 0) {
+      $(charCount).css('color', 'red');
+      
+    } else {
+      $(charCount).css('color', '#545149');
+    }
+    
+    //console.log("this is the count: ",$(charCount).text());
+  });
+});
